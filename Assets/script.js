b7618510d121f7a56260c9a48144a9ea103fef37
[Andres Perez] <mandresperez.uk@gmail.com>
2022-11-02 20:01:37 -0600

    reformatted HTML to remove unnecessary buttons and built out the js for choosing a question at the start of the game, as well as checking if the answer is correct

--- Assets/script.js
+++ Assets/script.js
@@ -1,3 +1,15 @@
+var startButton = document.querySelector('#start')
+// This links startButton to the start quiz button in html
+var questionBoxEl = document.querySelector('#questionBox')
+// This links our questionBox section to the var
+var questionTitle = document.querySelector('#titleText')
+var a = document.querySelector('.choice-1')
+var b = document.querySelector('.choice-2')
+var c = document.querySelector('.choice-3')
+var d = document.querySelector('.choice-4')
+var currentIndex = 0
+var correct = 0
+var incorrect = 0
 var questions = [
     {
         title: "Why is it this way?",
@@ -28,14 +40,54 @@ var questions = [
 ]
 // These are all of the questions that we have in the test as well as the choices you can select and the correct answer of those choices
 
-var startButton = document.getElementById('start')
-// This links startButton to the start quiz button in html
-var questionBoxEl = document.getElementById('questionBox')
-// This links our questionBox section to the var
-
-startButton.addEventListener('click', startQuiz);
-// This is an event listener on the button that executes the function start quiz when the button is clicked
-
 function startQuiz() {
     alert('Begin!');
+    chooseQuestion()
 }
+
+function chooseQuestion() {
+    for (let i = 0; i < questions.length; i++) {
+        questionTitle.innerText = questions[currentIndex].title
+        a.innerText = questions[currentIndex].choices[0]
+        b.innerText = questions[currentIndex].choices[1]
+        c.innerText = questions[currentIndex].choices[2]
+        d.innerText = + questions[currentIndex].choices[3]
+    }
+}
+
+function checkAnswer() {
+    if (event.target.innerText === questions[currentIndex].answer) {
+        alert("Correct!");
+        correct = correct + 1
+        nextQuestion();
+    } else {
+        alert("Incorrect!");
+        incorrect = incorrect + 1
+        nextQuestion();
+    }
+}
+
+function nextQuestion() {
+
+}
+
+// I need an event listener for selections on the 4 choice buttons 
+
+a.addEventListener('click', function(event) {
+    console.log(event.target.innerText);
+    checkAnswer();
+})
+b.addEventListener('click', function(event) {
+    console.log(event.target.innerText);
+    checkAnswer();
+})
+c.addEventListener('click', function(event) {
+    console.log(event.target.innerText);
+    checkAnswer();
+})
+d.addEventListener('click', function(event) {
+    console.log(event.target.innerText);
+    checkAnswer();
+})
+// This is an event listener on the button that executes the function start quiz when the button is clicked
+startButton.addEventListener('click', startQuiz);
